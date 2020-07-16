d4be6747919abbb65f08d026a14c83a91f8d0cce
Mangesh Jadhav <149mpj@gmail.com>
2020-07-16 23:58:28 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl,FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FormGroup';
+  data;
+  name:string;
+  email:string;
+  phone:string;
+
+  profileForm = new FormGroup({
+    fgname : new FormControl(''),
+    fgemail : new FormControl(''),
+    fgphone : new FormControl('')
+  });
+
+  Submitdata(){
+    console.log(this.profileForm.value);
+    this.data = this.profileForm.value;
+    console.log(this.profileForm.controls);
+  }
 }
